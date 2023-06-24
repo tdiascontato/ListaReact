63a71236028b9c6bd4b4d5525c6816224f7b1726
Tiago Dias <98658691+tdiascontato@users.noreply.github.com>
2023-06-24 18:23:39 -0300

    Add files via upload

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,17 +6,74 @@ export default class Main extends Component{
 
   state = {
     novaTarefa: '',
-    tarefas:[
-      'Estudar React',
-      'Fazer currículo',
-      'Fazer site'
-    ]
+    tarefas:[],
+    index: -1
   };
+
+  componentDidMount(){
+    const tarefas = JSON.parse(localStorage.getItem('tarefas'));
+
+    if(!tarefas) return;
+
+    this.setState({tarefas});
+  }
+
+  componentDidUpdate(prevProps, prevState){
+    const {tarefas} = this.state;
+
+    if(tarefas === prevState.tarefas) return;
+
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+  }
+
+  handleSubmit =(e) => {
+    e.preventDefault();
+    const {tarefas, index} = this.state;
+    let {novaTarefa} = this.state;
+    novaTarefa = novaTarefa.trim();//sem espaços
+
+    if(tarefas.indexOf(novaTarefa) !== -1) return;//conferindo se novaTarefa == último el da lista
+
+    const novasTarefas = [...tarefas];//copiei do state
+
+    if(index === -1){
+      this.setState({
+        tarefas: [...novasTarefas, novaTarefa],
+        novaTarefa: ''
+      });
+    } else {
+      novasTarefas[index] = novaTarefa;
+
+      this.setState({
+        tarefas: [...novasTarefas],
+        index: -1
+      });
+    }
+  }
 
   handleChange = (e) => {
     this.setState({
       novaTarefa: e.target.value
     });
+  }
+
+  handleEdit =(e, index)=>{
+    const {tarefas} = this.state;
+
+    this.setState({
+      index,
+      novaTarefa: tarefas[index]
+    })
+  }
+
+  handleDelete =(e, index)=>{
+    const {tarefas} = this.state;
+    const novasTarefas = [...tarefas];
+    novasTarefas.splice(index,1);
+
+    this.setState({
+      tarefas: [...novasTarefas]
+    })
   }
 
   render(){
@@ -27,19 +84,19 @@ export default class Main extends Component{
 
         <h1> Lista de Tarefas:</h1><br/>
 
-        <form action='#' className='form'>
+        <form onSubmit={this.handleSubmit} action='#' className='form'>
           <input onChange={this.handleChange} type="text" value = {novaTarefa}/>
           <button type="submit"><FaPlusCircle /> </button>
         </form>
 
         <ul className="tarefas">
-          {tarefas.map(tarefa => (
+          {tarefas.map((tarefa, index) => (
             <li key={tarefa}>
               {tarefa}
-              <div>
-                <FaEdit className='edit' />
-                <FaWindowClose className='delete' />
-              </div>
+              <span>
+                <FaEdit onClick={(e) => this.handleEdit(e, index)} className='edit' />
+                <FaWindowClose onClick={(e) => this.handleDelete(e, index)} className='delete' />
+              </span>
             </li>
           ))}
         </ul>
